d7abd096bde96e5e29c78b842c747b5f3bcaa81f
iletisimzafer52@hotmail.com <iletisimzafer52@hotmail.com>
2024-02-18 02:33:12 +0300

    anasayfa

--- resources/views/front/partials/navbar.blade.php
+++ resources/views/front/partials/navbar.blade.php
@@ -3,27 +3,25 @@
         <div class="container">
             <nav class="navbar navbar-dark navbar-expand-lg py-0">
                 <a href="index.html" class="navbar-brand">
-                    <h1 class="text-white fw-bold d-block">High<span class="text-secondary">Tech</span> </h1>
+                    <h1 class="text-white fw-bold d-block">Basit<span class="text-secondary">CRM</span> </h1>
                 </a>
                 <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse bg-transparent" id="navbarCollapse">
                     <div class="navbar-nav ms-auto mx-xl-auto p-0">
-                        <a href="index.html" class="nav-item nav-link active text-secondary">Home</a>
-                        <a href="about.html" class="nav-item nav-link">About</a>
-                        <a href="service.html" class="nav-item nav-link">Services</a>
-                        <a href="project.html" class="nav-item nav-link">Projects</a>
+                        <a href="index.html" class="nav-item nav-link active text-secondary">Anasayfa</a>
+                        <a href="about.html" class="nav-item nav-link">Hakkımızda</a> 
+                        <a href="project.html" class="nav-item nav-link">Projeler</a>
                         <div class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Sayfalar</a>
                             <div class="dropdown-menu rounded">
-                                <a href="blog.html" class="dropdown-item">Our Blog</a>
-                                <a href="team.html" class="dropdown-item">Our Team</a>
-                                <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                                <a href="404.html" class="dropdown-item">404 Page</a>
+                                <a href="blog.html" class="dropdown-item">Blog</a>
+                                <a href="team.html" class="dropdown-item">Bizim Ekip</a>
+                                <a href="testimonial.html" class="dropdown-item">Müşteri Yorumları</a> 
                             </div>
                         </div>
-                        <a href="contact.html" class="nav-item nav-link">Contact</a>
+                        <a href="contact.html" class="nav-item nav-link">İletişim</a>
                     </div>
                 </div>
                 <div class="d-none d-xl-flex flex-shirink-0">
@@ -36,8 +34,8 @@
                         </a>
                     </div>
                     <div class="d-flex flex-column pe-4 border-end">
-                        <span class="text-white-50">Have any questions?</span>
-                        <span class="text-secondary">Call: + 0123 456 7890</span>
+                        <span class="text-white-50">Sorularınız mı var ?</span>
+                        <span class="text-secondary">Telefon: +90 539 314 19 74</span>
                     </div>
                     <div class="d-flex align-items-center justify-content-center ms-4 ">
                         <a href="#"><i class="bi bi-search text-white fa-2x"></i> </a>
--- resources/views/front/partials/slider.blade.php
+++ resources/views/front/partials/slider.blade.php
@@ -10,11 +10,11 @@
                     <img src="tema/img/carousel-1.jpg" class="img-fluid" alt="First slide">
                     <div class="carousel-caption">
                         <div class="container carousel-content">
-                            <h6 class="text-secondary h4 animated fadeInUp">Best IT Solutions</h6>
-                            <h1 class="text-white display-1 mb-4 animated fadeInRight">An Innovative IT Solutions Agency</h1>
-                            <p class="mb-4 text-white fs-5 animated fadeInDown">Lorem ipsum dolor sit amet elit. Sed efficitur quis purus ut interdum. Pellentesque aliquam dolor eget urna ultricies tincidunt.</p>
-                            <a href="" class="me-2"><button type="button" class="px-4 py-sm-3 px-sm-5 btn btn-primary rounded-pill carousel-content-btn1 animated fadeInLeft">Read More</button></a>
-                            <a href="" class="ms-2"><button type="button" class="px-4 py-sm-3 px-sm-5 btn btn-primary rounded-pill carousel-content-btn2 animated fadeInRight">Contact Us</button></a>
+                            <h6 class="text-secondary h4 animated fadeInUp">Müşteri Yönetimi</h6>
+                            <h1 class="text-white display-1 mb-4 animated fadeInRight">Müşterilerinizi Takip Edin</h1>
+                            <p class="mb-4 text-white fs-5 animated fadeInDown">Müşteri Hareketlerine Göre Aksiyon Alın.</p>
+                            <a href="" class="me-2"><button type="button" class="px-4 py-sm-3 px-sm-5 btn btn-primary rounded-pill carousel-content-btn1 animated fadeInLeft">Daha Fazla</button></a>
+                            <a href="" class="ms-2"><button type="button" class="px-4 py-sm-3 px-sm-5 btn btn-primary rounded-pill carousel-content-btn2 animated fadeInRight">Demo Talep Et</button></a>
                         </div>
                     </div>
                 </div>
@@ -22,11 +22,11 @@
                     <img src="tema/img/carousel-2.jpg" class="img-fluid" alt="Second slide">
                     <div class="carousel-caption">
                         <div class="container carousel-content">
-                            <h6 class="text-secondary h4 animated fadeInUp">Best IT Solutions</h6>
-                            <h1 class="text-white display-1 mb-4 animated fadeInLeft">Quality Digital Services You Really Need!</h1>
-                            <p class="mb-4 text-white fs-5 animated fadeInDown">Lorem ipsum dolor sit amet elit. Sed efficitur quis purus ut interdum. Pellentesque aliquam dolor eget urna ultricies tincidunt.</p>
-                            <a href="" class="me-2"><button type="button" class="px-4 py-sm-3 px-sm-5 btn btn-primary rounded-pill carousel-content-btn1 animated fadeInLeft">Read More</button></a>
-                            <a href="" class="ms-2"><button type="button" class="px-4 py-sm-3 px-sm-5 btn btn-primary rounded-pill carousel-content-btn2 animated fadeInRight">Contact Us</button></a>
+                            <h6 class="text-secondary h4 animated fadeInUp">Satış Yönetimi</h6>
+                            <h1 class="text-white display-1 mb-4 animated fadeInLeft">Satışları, Projeleri tek bir alandan yönetin!</h1>
+                            <p class="mb-4 text-white fs-5 animated fadeInDown">Müşterilerle kontakt kurun. Ve BasitCRM ile bu süreci yönetin </p>
+                            <a href="" class="me-2"><button type="button" class="px-4 py-sm-3 px-sm-5 btn btn-primary rounded-pill carousel-content-btn1 animated fadeInLeft">Daha Fazla</button></a>
+                            <a href="" class="ms-2"><button type="button" class="px-4 py-sm-3 px-sm-5 btn btn-primary rounded-pill carousel-content-btn2 animated fadeInRight">Demo Talep Et</button></a>
                         </div>
                     </div>
                 </div>
