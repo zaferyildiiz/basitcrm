6be83ae1b3c97dfac5b2ecd8f6bd770adca890bc
iletisimzafer52@hotmail.com <iletisimzafer52@hotmail.com>
2024-02-28 00:45:02 +0300

    panel

--- database/migrations/2024_02_27_210033_create_customers_table.php
+++ database/migrations/2024_02_27_210033_create_customers_table.php
@@ -12,8 +12,30 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('customers', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->bigIncrements('customer_id');
+            $table->string('customer_name');
+            $table->string('customer_type');
+            $table->string('city');
+            $table->string('district');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('tax_no')->nullable();
+            $table->string("tax_office")->nullable();
+            $table->string('contact_person_name');
+            $table->string('contact_person_email');
+            $table->string('contact_person_phone');
+            $table->string('activity_area');
+            $table->string('customer_note');
+            $table->integer('company_id');
+            $table->datetime("created_at");
+            $table->datetime("updated_at");
+            $table->datetime("deleted_at")->nullable();
+            $table->integer("created_by");
+            $table->integer("updated_by");
+            $table->integer("deleted_by")->nullable();
+
+
         });
     }
 
